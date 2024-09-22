cf8901184e062bfc58c77b597d9e9e981ebb8062
NjSaint29 <njudethaddaeus@gmail.com>
2024-09-22 01:34:08 +0100

    general

---
+++ script.js
@@ -0,0 +1,8 @@
+// JS (script.js)
+
+// Initialize Bootstrap
+$(document).ready(function() {
+    $('.navbar-toggler').click(function() {
+        $('.navbar-collapse').toggleClass('show');
+    });
+});
